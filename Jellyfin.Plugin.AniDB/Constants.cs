acfec21980f89f5b85ea19e360f688a31332b8d2
Odd Stråbø <oddstr13@openshell.no>
2020-12-14 23:26:18 +0100

    Update plugin name

--- Jellyfin.Plugin.AniDB/Constants.cs
+++ Jellyfin.Plugin.AniDB/Constants.cs
@@ -2,7 +2,7 @@ namespace Jellyfin.Plugin.AniDB
 {
     public static class Constants
     {
-        public const string PluginName = "Anime";
+        public const string PluginName = "AniDB";
         public const string PluginGuid = "a2b2a7ed-aa28-4521-a64a-63d86901f246";
     }
 }
